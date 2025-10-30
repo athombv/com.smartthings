ea39a589549ad5072b6c3b8ba91e033a21e80de7
Emile Nijssen <emile@emile.nl>
2025-10-30 17:46:03 +0100

    wip

--- drivers/dishwasher/device.js
+++ drivers/dishwasher/device.js
@@ -6,9 +6,18 @@ module.exports = class SmartThingsDeviceDishwasher extends SmartThingsDevice {
 
   static CAPABILITIES = [
     {
-      homeyCapabilityId: 'samsung_dishwasher_status_job',
+      homeyCapabilityId: 'samsung_dishwasher_washing_course',
       smartThingsComponentId: 'main',
-      smartThingsCapabilityId: 'dishwasherOperatingState',
+      smartThingsCapabilityId: 'samsungce.dishwasherWashingCourse',
+      smartThingsAttributeId: 'washingCourse',
+      async onReport({ value }) {
+        return value;
+      },
+    },
+    {
+      homeyCapabilityId: 'samsung_dishwasher_current_job_state',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'samsungce.dishwasherJobState',
       smartThingsAttributeId: 'dishwasherJobState',
       async onReport({ value }) {
         if (value === 'finished') {
@@ -19,6 +28,28 @@ module.exports = class SmartThingsDeviceDishwasher extends SmartThingsDevice {
         }
 
         return value;
+      },
+    },
+    {
+      homeyCapabilityId: 'alarm_contact.dishwasher_door',
+      smartThingsComponentId: 'onedoor',
+      smartThingsCapabilityId: 'contactSensor',
+      smartThingsAttributeId: 'contact',
+      async onReport({ value }) {
+        if (value === 'open') return true;
+        if (value === 'closed') return false;
+        return null;
+      },
+    },
+    {
+      homeyCapabilityId: 'samsung_dishwasher_remote_controller_enabled',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'remoteControlStatus',
+      smartThingsAttributeId: 'remoteControlEnabled',
+      async onReport({ value }) {
+        if (value === 'true') return true;
+        if (value === 'false') return false;
+        return null;
       },
     },
   ];
