dee4f7274b72c3f52241ecc3b503f0e9e6595d60
Emile Nijssen <emile@emile.nl>
2025-11-10 11:44:40 +0100

    add 'The Freestyle'

---
+++ drivers/freestyle/device.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const SmartThingsDevice = require('../../lib/SmartThingsDevice');
+
+module.exports = class SmartThingsDeviceTheFreestyle extends SmartThingsDevice {
+
+  static CAPABILITIES = [
+    {
+      homeyCapabilityId: 'onoff',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'switch',
+      smartThingsAttributeId: 'switch',
+      async onSet({ value }) {
+        await this.executeCommand({
+          component: 'main',
+          capability: 'switch',
+          command: value
+            ? 'on'
+            : 'off',
+        });
+      },
+      async onReport({ value }) {
+        return value === 'on';
+      },
+    },
+    {
+      homeyCapabilityId: 'volume_up',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'audioVolume',
+      async onSet() {
+        await this.executeCommand({
+          component: 'main',
+          capability: 'audioVolume',
+          command: 'volumeUp',
+        });
+      },
+    },
+    {
+      homeyCapabilityId: 'volume_down',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'audioVolume',
+      async onSet() {
+        await this.executeCommand({
+          component: 'main',
+          capability: 'audioVolume',
+          command: 'volumeDown',
+        });
+      },
+    },
+    {
+      homeyCapabilityId: 'channel_up',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'tvChannel',
+      async onSet() {
+        await this.executeCommand({
+          component: 'main',
+          capability: 'tvChannel',
+          command: 'channelUp',
+        });
+      },
+    },
+    {
+      homeyCapabilityId: 'channel_down',
+      smartThingsComponentId: 'main',
+      smartThingsCapabilityId: 'tvChannel',
+      async onSet() {
+        await this.executeCommand({
+          component: 'main',
+          capability: 'tvChannel',
+          command: 'channelDown',
+        });
+      },
+    },
+  ];
+
+};
---
+++ drivers/freestyle/driver.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const SmartThingsDriver = require('../../lib/SmartThingsDriver');
+
+module.exports = class SmartThingsDriverTheFreestyle extends SmartThingsDriver {
+
+  async onOAuth2Init() {
+    await super.onOAuth2Init();
+
+    this.homey.flow
+      .getActionCard('samsung_freestyle_set_volume')
+      .registerRunListener(async ({ device, volume }) => {
+        return device.executeCommand({
+          capability: 'audioVolume',
+          command: 'setVolume',
+          args: [Math.round(volume * 100)],
+        });
+      });
+
+    this.homey.flow
+      .getActionCard('samsung_freestyle_set_volume_mute')
+      .registerRunListener(async ({ device }) => {
+        return device.executeCommand({
+          component: 'main',
+          capability: 'audioMute',
+          command: 'mute',
+        });
+      });
+
+    this.homey.flow
+      .getActionCard('samsung_freestyle_set_volume_unmute')
+      .registerRunListener(async ({ device }) => {
+        return device.executeCommand({
+          component: 'main',
+          capability: 'audioMute',
+          command: 'unmute',
+        });
+      });
+
+    // We haven't found a TV that supports the `samsungTV` capability yet.
+    // this.homey.flow
+    //   .getActionCard('show_message')
+    //   .registerRunListener(async ({ device, message }) => {
+    //     return device.onFlowActionShowMessage({ message });
+    //   });
+    //
+    // driver.flow.compose.json:
+    // {
+    //   "id": "show_message",
+    //   "title": {
+    //     "en": "Show a message"
+    //   },
+    //   "titleFormatted": {
+    //     "en": "Show message [[message]] on TV"
+    //   },
+    //   "args": [
+    //     {
+    //       "name": "message",
+    //       "type": "text",
+    //       "title": {
+    //         "en": "Message"
+    //       },
+    //       "placeholder": {
+    //         "en": "Hello from Homey!"
+    //       }
+    //     }
+    //   ]
+    // },
+  }
+
+  onPairFilterDevice(device) {
+    this.log('onPairFilterDevice', device.deviceTypeName);
+
+    if (device.deviceTypeName === 'x.com.st.d.projector') return true;
+    return false;
+  }
+
+};
